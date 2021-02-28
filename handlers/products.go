489aa40e69fc569762bf3423011d3befaa10008f
Adam Dad <dada@thehutgroup.com>
2021-02-28 19:13:54 +0000

    initial commit

---
+++ handlers/products.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type Products struct {
+	logger *log.Logger
+}
+
+func NewProducts(logger *log.Logger) *Products {
+	return &Products{logger: logger}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Response) {
+	p.logger.Println("hello world")
+
+	d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "something went wrong", http.StatusBadRequest)
+			return
+		}
+
+	fmt.Fprintf(rw, "%s\n", d)
+}
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"github.com/adamdadd/go-products-microservice/handlers"
+)
+
+func main() {
+	l := log.New(os.Stdout, "Product API", log.LstdFlags)
+	hh := handlers
+	http.HandleFunc("/", hh)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
